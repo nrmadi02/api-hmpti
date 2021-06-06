499043dbe4c1483aefff24c71f7c3ac6fcdfc7e2
Nur Ahmadi <ahmadinur2202@gmail.com>
2021-06-06 15:25:00 +0800

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fca9706fff0458172319777f96337b64'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b636696-b136-4cac-a575-6f33ceb7fcbb'
+};
